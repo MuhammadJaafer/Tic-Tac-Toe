f52cd155142ef504cb1d50091b77cfd4f7e2216e
Muhammad Jaafar <mohamadjafer14235@gmail.com>
2022-09-05 18:37:04 +0300

    setup the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import "./App.css";
-
 function App() {
-  return <div className="App">App`</div>;
+  return (
+    <div className="App">
+      <h1>
+        <span className="blue-text-glow">Tic </span>
+        <span className="red-text-glow">Tac </span>
+        <span className="blue-text-glow">Toe </span>
+      </h1>
+    </div>
+  );
 }
 
 export default App;
