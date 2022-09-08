f5424d71de4c001cb076c4b2018626a6180cc171
Muhammad Jaafar <mohamadjafer14235@gmail.com>
2022-09-08 03:06:49 +0300

    almost finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Borde from "./components/Borde";
 const initBorde = ["", "", "", "", "", "", "", "", ""];
 function App() {
@@ -6,26 +6,92 @@ function App() {
   const [turn, setTurn] = useState("player1");
   const [player1Score, setPlayer1Score] = useState(0);
   const [player2Score, setPlayer2Score] = useState(0);
+  const [click, setclick] = useState(0);
+  const [win, setWin] = useState(null);
+  const [winLine, setWinLine] = useState(null);
 
+  //check if some one win
+  const Check = () => {
+    const win = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [2, 4, 6],
+      [0, 4, 8],
+    ];
+    win.map((line) => {
+      if (
+        borde[line[0]] === "x" &&
+        borde[line[1]] === "x" &&
+        borde[line[2]] === "x"
+      ) {
+        setWin("x");
+        setWinLine(line);
+      }
+      if (
+        borde[line[0]] === "o" &&
+        borde[line[1]] === "o" &&
+        borde[line[2]] === "o"
+      ) {
+        setWin("o");
+        setWinLine(line);
+      }
+    });
+  };
+
+  //delay function
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  //restart game
+  const RestartGame = async () => {
+    if (win === "x") {
+      console.log(winLine, "X");
+    } else if (win === "o") {
+      console.log(winLine, "O");
+    } else {
+      console.log("Drow");
+    }
+    await delay(1000);
+    setBorde(initBorde);
+    setTurn("player1");
+    setclick(0);
+    setWin(null);
+    setWinLine(null);
+  };
+
+  // handle Clicks on the borde
   const handleClick = (e) => {
     const index = e.target.getAttribute("index");
-    if (!index) {
+
+    if (borde[index] != "" || click >= 9) {
       console.log("already taken");
       return;
     }
 
-    console.log(index, turn);
+    setclick(click + 1);
 
     const newBorde = [...borde];
     newBorde[index] = turn === "player1" ? "x" : "o";
     setBorde(newBorde);
+
     setTurn((prev) => {
       if (prev === "player1") return "player2";
       else return "player1";
     });
   };
 
-  console.log(borde);
+  useEffect(() => {
+    Check();
+    if (win) {
+      win === "x"
+        ? setPlayer1Score(player1Score + 1)
+        : setPlayer2Score(player2Score + 1);
+      RestartGame();
+    } else if (click >= 9) RestartGame();
+  }, [win, click]);
 
   return (
     <div className="App">
@@ -38,12 +104,12 @@ function App() {
       <div className="score">
         <div className="player-score">
           <div>Player 1</div>
-          <div className="player1-score">{player1Score}</div>
+          <div className="player1-score blue-text-glow">{player1Score}</div>
         </div>
         <div className="text-score">Score</div>
         <div className="player-score">
           <div>Player 2</div>
-          <div className="player2-score">{player2Score}</div>
+          <div className="player2-score red-text-glow">{player2Score}</div>
         </div>
       </div>
     </div>
